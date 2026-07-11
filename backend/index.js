bad8868a0b5cc45430b1e87e14f8abc76dce5433
biohazards1108-lab <biohazards1108@gmail.com>
2026-07-11 15:31:51 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Backend is live!");
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend connection successful!" });
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
