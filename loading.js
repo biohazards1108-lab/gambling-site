2e856c11e2de8dced3cd23dc97c4562a3c20fe60
biohazards1108-lab <biohazards1108@gmail.com>
2026-07-12 18:20:15 -0700

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const token = localStorage.getItem("token");
 
+    // Delay so loading screen actually shows
     setTimeout(() => {
         if (!token) {
             window.location.href = "login.html";
         } else {
             window.location.href = "game/index.html";
         }
-    }, 2000); // 2-second loading animation
+    }, 4500); // 4.5 seconds
 });
